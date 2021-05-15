e44a766d4de42bfacc1245edde9f869396ac0036
Roman Kulyk <roman.kulyk@ucu.edu.ua>
2021-05-15 22:30:25 +0300

    too many requests lol we need to take a break that's just a backup in case of messing up

--- ArticleADT.py
+++ ArticleADT.py
@@ -3,54 +3,24 @@
 
 from GoogleNews import GoogleNews
 
+
 class ArticleADT:
     """A class for representing a news article."""
-    def __init__(self, politician_name, number):
+
+    def __init__(self, politician_name):
         """Initialize an object representing an article."""
         news = GoogleNews()
         news.setlang("uk")
         news.setencode("utf-8")
         news.setperiod("3d")
-
         news.search(politician_name)
+        info = news.result()
+        self.articles = []
+        for i in range(5):
+            text = info[i]
+            info_list = [text["title"], text["link"]]
+            self.articles.append(info_list)
 
-        self.info = news.result()[number]
 
-        self.article = {}
-
-    def set_date_time(self):
-        self.article["date_time"] = self.info["date"]
-
-    def set_description(self):
-        self.article["description"] = self.info["desc"]
-
-    def set_link(self):
-        self.article["url"] = self.info["link"]
-
-    def set_title(self):
-        self.article["title"] = self.info["title"]
-
-    def set_media(self):
-        self.article["media"] = self.info["media"]
-
-    def set_image(self):
-        self.article["image"] = self.info["img"]
-
-    def __str__(self):
-        """Return information about an article as a string of key-value pairs."""
-        string = ""
-        if self.article.keys:
-            for key in self.article.keys():
-                string += key + ": " + self.article[key] + "\n"
-        
-        return string
-
-# news = ArticleADT("Володимир Зеленський", 0)
-# news.set_date_time()
-# news.set_title()
-# news.set_link()
-# news.set_image()
-# news.set_media()
-# news.set_description()
-# print(news)
-# print(type(news))
+article = ArticleADT('Віктор Медведчук')
+print(article)
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from WikiADT import WikiADT
 from flask import Flask, render_template, request, redirect
 from ArticleADT import ArticleADT
 
@@ -15,15 +16,15 @@ def page():
 def analyze():
     name = request.form["name_surname"]
 
-    for i in range(4):
-        text = ArticleADT(name, i)
-        text.set_date_time()
-        text.set_title()
-        text.set_description()
-        text.set_media()
-        articles.append(str(text))
+    wiki_object = WikiADT(name)
+    articles = ArticleADT(name)
 
-    return render_template("success.html", politician_name=name, articles=articles)
+    return render_template("result.html", image=wiki_object.links[1], name=name, politician_description=wiki_object.wiki_desc,
+                           desc_1=articles[0][0], link_1=articles[0][1],
+                           desc_2=articles[1][0], link_2=articles[1][1],
+                           desc_3=articles[2][0], link_3=articles[2][1],
+                           desc_4=articles[3][0], link_4=articles[3][1],
+                           desc_5=articles[4][0], link_5=articles[4][1])
 
 
 if __name__ == "__main__":
