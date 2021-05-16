1ae0868efb91e96abad9e7f29054dcdc2cee082a
Bohdan Ruban <bodya.ru02@gmail.com>
2021-05-16 11:58:05 +0300

    Add line chart to result.html

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from WikiADT import WikiADT
 from flask import Flask, render_template, request, redirect
 from ArticleADT import ArticleADT
 from WikiADT import WikiADT
+from modules.trends import Trends
 
 app = Flask(__name__)
 
@@ -20,16 +21,20 @@ def analyze():
 
     wiki_object = WikiADT(name)
 
-    articles = ArticleADT(name)
+    articles = ArticleADT(name).articles
+
+    trends = Trends([name.lower()])
+    interest = trends.interest_over_time()
+    dates = [str(i).split('T')[0] for i in list(interest.index.values)][-20:]
+    popularity_level = interest[name.lower()].tolist()[-20:]
 
     return render_template("result.html", image=wiki_object.links[1], name=name, politician_description=wiki_object.wiki_desc,
                            desc_1=articles[0][0], link_1=articles[0][1],
                            desc_2=articles[1][0], link_2=articles[1][1],
                            desc_3=articles[2][0], link_3=articles[2][1],
                            desc_4=articles[3][0], link_4=articles[3][1],
-                           desc_5=articles[4][0], link_5=articles[4][1])
-
+                           desc_5=articles[4][0], link_5=articles[4][1], popularity_level=popularity_level, dates=dates)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ modules/trends.py
@@ -0,0 +1,52 @@
+'''
+Module for working with Google Trends.
+'''
+
+from pytrends.request import TrendReq
+import pandas as pd
+
+
+class Trends:
+    def __init__(self, keywords):
+        self.pytrends = TrendReq(hl='uk', tz=360)
+        self.pytrends.build_payload(keywords)
+
+    def interest_over_time(self):
+        return self.pytrends.interest_over_time()
+
+    def interest_by_region(self, resolution='COUNTRY', inc_low_vol=False):
+        return self.pytrends.interest_by_region(resolution=resolution, inc_low_vol=inc_low_vol)
+
+    def related_topics(self):
+        return self.pytrends.related_topics()
+
+    def related_querries(self):
+        return self.pytrends.related_queries()
+
+    def trending_searches(self, pn='ukraine'):
+        return self.pytrends.trending_searches(pn=pn)
+
+    def top_charts(self, date, hl='uk', tz=360, geo='GLOBAL'):
+        return self.pytrends.top_charts(date, hl=hl, tz=tz, geo=geo)
+
+    def suggestions(self, keyword):
+        return self.pytrends.suggestions(keyword)
+
+
+if __name__ == '__main__':
+    # trends = Trends(['Зеленський', 'Zelensky', 'Trump'])
+    trends = Trends(['зеленський'])
+    # trends = Trends(['медведчук'])
+    interest = trends.interest_over_time()
+    # print(trends.interest_by_region())
+    # print(trends.related_topics())
+    # print(trends.related_querries())
+    # print(trends.trending_searches())
+    # print(trends.suggestions('зеленський'))
+    # dates = [str(i).split('T')[0] for i in list(interest.index.values)]
+    # print(dates)
+    # print(interest)
+    # print(interest['медведчук'].tolist())
+    # print(interest['Name'])
+    # print(trends.trending_searches('ukraine'))
+    # print(trends.top_charts(202012))
